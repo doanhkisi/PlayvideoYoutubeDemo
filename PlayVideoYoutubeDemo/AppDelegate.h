386ad4b1e7abcb1f6933fd3740cbc7f34e72c03a
Bui Duy Doanh <doanhkisi@doanhkisi.local>
2015-03-23 14:59:33 +0700

    Initial Commit

---
+++ PlayVideoYoutubeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayVideoYoutubeDemo
+//
+//  Created by Bui Duy Doanh on 3/23/15.
+//  Copyright (c) 2015 doanhkisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayVideoYoutubeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayVideoYoutubeDemo
+//
+//  Created by Bui Duy Doanh on 3/23/15.
+//  Copyright (c) 2015 doanhkisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
